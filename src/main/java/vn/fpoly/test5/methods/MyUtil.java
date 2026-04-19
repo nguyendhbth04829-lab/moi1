132fbc1863ec0a7221131b3de7400b8081817b0e
Nguyen <emailcuaban@gmail.com>
2026-04-19 21:34:43 +0700

    addseleemukmyutil

--- src/main/java/vn/fpoly/test5/methods/MyUtil.java
+++ src/main/java/vn/fpoly/test5/methods/MyUtil.java
@@ -1,4 +1,14 @@
 package vn.fpoly.test5.methods;
 
 public class MyUtil {
+public int tinhtongtu1denn(int n){
+    if (n<1){
+        throw new IllegalArgumentException("n phai lon hon 1");
+    }
+    int tong= 0;
+    for (int i = 1; i<=n;i++){
+        tong += i;
+    }
+    return tong;
+}
 }
